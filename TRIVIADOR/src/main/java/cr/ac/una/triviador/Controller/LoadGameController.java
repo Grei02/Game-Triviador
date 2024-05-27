55a6cb6239aea7b7f1821c01c00342158965bda2
SofiaBejaranoMora <aslyn.bejarano.mora@est.una.ac.cr>
2024-05-27 01:55:18 -0600

    the ids were made

--- TRIVIADOR/src/main/java/cr/ac/una/triviador/Controller/LoadGameController.java
+++ TRIVIADOR/src/main/java/cr/ac/una/triviador/Controller/LoadGameController.java
@@ -4,23 +4,71 @@
  */
 package cr.ac.una.triviador.Controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.materialfx.controls.legacy.MFXLegacyTableView;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Sofia Bejarano Mora
- */
 public class LoadGameController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private MFXButton btnAdd;
+
+    @FXML
+    private MFXButton btnCancel;
+
+    @FXML
+    private MFXButton btnChangeName;
+
+    @FXML
+    private MFXButton btnEliminate;
+
+    @FXML
+    private MFXButton btnSave;
+
+    @FXML
+    private MFXLegacyTableView<?> tabRegistered;
+
+    @FXML
+    private MFXTextField txtName;
+
+    @FXML
+    void onActionBtnAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnChangeName(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnEliminate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionTxtName(ActionEvent event) {
+
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
     }    
     
 }
