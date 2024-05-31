6e73169d5a34f3dbe4d03ffd1161e4a80a1f7630
Greichel Campos Jiménez <greichel.campos.jimenez@est.una.ac.cr>
2024-05-31 01:49:35 -0600

    agregar

--- TRIVIADOR/src/main/java/cr/ac/una/triviador/Controller/PrincipalViewController.java
+++ TRIVIADOR/src/main/java/cr/ac/una/triviador/Controller/PrincipalViewController.java
@@ -3,9 +3,30 @@ package cr.ac.una.triviador.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 
 public class PrincipalViewController extends Controller implements Initializable {
+
+    @FXML
+    private MenuItem itemLoadGame;
+    @FXML
+    private MenuItem itemStartGame;
+    @FXML
+    private MenuItem itemAbout;
+    @FXML
+    private MenuItem itemHowToPlay;
+    @FXML
+    private MenuItem itemSeeRanking;
+    @FXML
+    private MenuItem itemSeeProfile;
+    @FXML
+    private MenuItem itemManageQuestions;
+    @FXML
+    private ImageView imgPrincipal;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
@@ -13,5 +34,33 @@ public class PrincipalViewController extends Controller implements Initializable
 
     @Override
     public void initialize() {
+    }
+
+    @FXML
+    private void onActionItemLoadGame(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActioniIemStartGame(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionItemAbout(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionItemHowToPlay(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionItemSeeRanking(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionItemSeeProfile(ActionEvent event) {
+    }
+
+    @FXML
+    private void onAxtionItemManageQuestions(ActionEvent event) {
     }
 }
--- TRIVIADOR/src/main/java/module-info.java
+++ TRIVIADOR/src/main/java/module-info.java
@@ -1,13 +1,34 @@
 module cr.ac.una.triviador {
     requires javafx.controls;
-    requires java.logging;
+    requires javafx.graphics;
+    requires javafx.base;
     requires javafx.fxml;
-    requires javafx.graphicsEmpty;
-
-    opens cr.ac.una.triviador to javafx.fxml;
-    exports cr.ac.una.triviador;
     requires MaterialFX;
-    requires java.base;
+    requires VirtualizedFX;
+    requires mfx.core;
+    requires mfx.localization;
+    requires mfx.effects;
+    requires mfx.resources;
+    requires com.oracle.database.jdbc;
+    //requires eclipselink;
     requires jakarta.persistence;
+   requires jakarta.xml.bind;
+    requires com.sun.tools.xjc;
+    requires com.sun.xml.bind.core;
+    requires org.eclipse.persistence.core;
+    requires org.eclipse.persistence.asm;
+    requires eclipselink.antlr;
+    requires org.eclipse.persistence.jpa;
+    requires org.eclipse.persistence.jpa.jpql;
+    requires org.eclipse.persistence.moxy;
+    requires java.persistence;
+    requires org.eclipse.persistence.jpa.modelgen.processor;
+    requires java.logging;
     
+    opens cr.ac.una.triviador to javafx.fxml;
+    opens cr.ac.una.triviador.controller to javafx.fxml;
+    //opens cr.ac.una.triviador.entities to jakarta.persistence, org.eclipse.persistence.core;
+    
+    exports cr.ac.una.triviador;
+    requires java.base;
 }
