d9cebb5db890f5cf4e820cbdc76c4f50463bd89a
SofiaBejaranoMora <aslyn.bejarano.mora@est.una.ac.cr>
2024-06-03 05:07:29 -0600

    ruleta y grados

--- TRIVIADOR/src/main/java/cr/ac/una/triviador/Controller/GameViewController.java
+++ TRIVIADOR/src/main/java/cr/ac/una/triviador/Controller/GameViewController.java
@@ -22,6 +22,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class GameViewController extends Controller implements Initializable {
@@ -31,27 +32,60 @@ public class GameViewController extends Controller implements Initializable {
     @FXML
     private ImageView imgGirar;
 
+    private Integer TotalRotationAngleRouletteImage = 0;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
+
+    }
 
     @Override
     public void initialize() {
-        
+
     }
 
     @FXML
     private void onMouseClickedGirar(MouseEvent event) {
+        rouletteMechanism();
+    }
+
+    private void rouletteMechanism() {
+        Random rand = new Random();
         RotateTransition rotateTransition = new RotateTransition();
         rotateTransition.setNode(imgRuleta);
-      //  rotateTransition.setDuration(Duration.ofSeconds(2)); // Duración de la animación
-        rotateTransition.setByAngle(360); // Ángulo de rotación
-        rotateTransition.setCycleCount(2); // Número de veces que se repite la animación
+        Integer AngleRouletteImage = rand.nextInt(1080) + 360;
+        //  rotateTransition.setDuration(Duration.ofSeconds(2)); // Duración de la animación
+        rotateTransition.setByAngle(AngleRouletteImage); // Ángulo de rotación
+        rotateTransition.setCycleCount(1); // Número de veces que se repite la animación
         rotateTransition.setAutoReverse(false); // No revertir la animación
-
+        TotalRotationAngleRouletteImage += AngleRouletteImage;
         rotateTransition.play();
-        
+        identifyCategoryAngle();
     }
-    
+
+    private void identifyCategoryAngle() {
+        double angle = 360 / 7;
+        System.out.println("total:" + TotalRotationAngleRouletteImage);
+        Integer auxTotalRotationAngleRouletteImage = TotalRotationAngleRouletteImage;
+        while (auxTotalRotationAngleRouletteImage >= 360) {
+            auxTotalRotationAngleRouletteImage -= 360;
+        }
+
+        if ((auxTotalRotationAngleRouletteImage >= 0) && (auxTotalRotationAngleRouletteImage <= angle)) {
+            System.out.println("Historia");
+        } else if (auxTotalRotationAngleRouletteImage <= (angle * 2)) {
+            System.out.println("Ciencia");
+        } else if (auxTotalRotationAngleRouletteImage <= (angle * 3)) {
+            System.out.println("Geografia");
+        } else if (auxTotalRotationAngleRouletteImage <= (angle * 4)) {
+            System.out.println("Corona");
+        } else if (auxTotalRotationAngleRouletteImage <= (angle * 5)) {
+            System.out.println("Entretenimiento");
+        } else if (auxTotalRotationAngleRouletteImage <= (angle * 6)) {
+            System.out.println("Arte");
+        } else if (auxTotalRotationAngleRouletteImage <= 360) {
+            System.out.println("Deporte");
+        }
+        System.out.println("total:" + TotalRotationAngleRouletteImage);
+    }
 }
